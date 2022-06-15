a41c531ed271fa773145cc635b3fd1a8d5e55a41
napoleanBonaparte <106796606+napoleanBonaparte@users.noreply.github.com>
2022-06-15 11:45:58 +0530

    Update arrayCalc.py

--- arrayCalc.py
+++ arrayCalc.py
@@ -1,21 +1,22 @@
 from scipy import stats
-
+#lmit of numbers
 limit = int(input("Enter the amount of numbers you need in  your array"))
 num_list = []
 #x = [1,2,3,4,5,6]
 x = 0
 y = 0
+#taking input
 for i in range(limit):
     num = int(input("Enter your number"))
     num_list.append(num)
-
+#the mean function
 def mean(c,div):
     div = int(len(num_list))
     for n in range(len(num_list)):
         c = c + num_list[n]
     result = c // div
     print(result, "is  your result")
-
+#the  median function
 def median(n):
     if len(num_list) % 2 !=0:
         n = (len(num_list)-1)/2
@@ -23,11 +24,11 @@ def median(n):
     else:
         n = (len(num_list))/2
         print("Your median value is:", num_list[n], "&",num_list[n+1])
-
+#the mode function
 def mode(lst):
      lst = stats.mode(num_list)
      print("mode value is ",lst)
-
+#calling the functions
 mean()
 median()
 mode()
